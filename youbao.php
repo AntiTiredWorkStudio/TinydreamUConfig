851bb04c344dea1f67eec2dae3b7380555b9defe
konglf2112 <konglf2112@foxmail.com>
2019-03-23 23:29:32 +0800

    增加友宝售货机互助详情页面及模板
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

---
+++ youbao.php
@@ -0,0 +1,24 @@
+<?php 
+$data = [
+	"template"=>"page01.html",
+	"bannerImgUrl"=>"https://tdream.antit.top/image/trade_ybshj.jpg",
+	"part01_title"=>"生意详情：",
+	"part01_subTitle01"=>"1.友宝简介",
+	"part01_subTitle02"=>"2.加盟优势",
+	"part01_subTitle03"=>"3.智慧运营",
+	"part01_subTitle04"=>"4.成功案例",
+	"part01_subTitle02ImgUrl"=>"../img/WechatIMG78.png",
+	"part01_subTitle03ImgUrl"=>"../img/WechatIMG79.png",
+	"part01_subTitle04ImgUrl"=>"../img/WechatIMG80.jpeg",
+	"part01_subContent01"=>"友宝成立于2011年，致力于将智能网络应用在自动售货机上，研发全球更先进的智能售货机，通过运用智能系统和云技术，开启产业变革，成为中国创新的售货机品牌。并在电子商务、本地生活服务、O2O等领域进行积极拓展，使个人消费者、合作伙伴都能从中获益。公司网址:http://www.ubox.cn/; 地址：北京市朝阳区广顺北大街5号院融创动力四层",
+	"part02_title"=>"生意收益分配：",
+	"part02_subTitle01"=>"1.项目获得者",
+	"part02_subTitle02"=>"2.利益分配",
+	"part02_subTitle03"=>"3.互助者项目收益",
+	"part02_subTitle04"=>"4.获得回报时间",
+	"part02_subContent01"=>"幸运者（即幸运者免费加盟价值2万元友宝售货机项目，平台工作人员会安排项目供应商与幸运者高效对接）；",
+	"part02_subContent02"=>"幸运者项目收益——项目运作后，初始产生的利润额度2万元 + 之后利润的50%；",
+	"part02_subContent03"=>"互助者平分项目“项目运作利润达到2万元之后”利润的50%；",
+	"part02_subContent04"=>"幸运者定期会做项目进展汇报，所有互助者均可以在平台幸运者详情页面查看。",
+];
+?>
