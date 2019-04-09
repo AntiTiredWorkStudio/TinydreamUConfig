b17e624f8d5e700ea206958c6a9e67b595fea683
whugobarry <17600740311@163.com>
2019-04-09 20:31:02 +0800

    代码修改

--- OperationData.php
+++ OperationData.php
@@ -2,8 +2,7 @@
 $data = [
 	'theme'=>[
 		'icon'=>["ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME"],
-		'text'=>["早起","看书","早睡","AB记单词","喝水","画画","练字","写日记"],
-		'obj'=>["{111:111}"]
+		'text'=>["早起","看书","早睡","AB记单词","喝水","画画","练字","写日记"]
 	]
 ];
 ?>
