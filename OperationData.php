d30724b69c1071e07739a45f19c16bf52d656d1b
whugobarry <17600740311@163.com>
2019-04-09 20:18:10 +0800

    代码修改

--- OperationData.php
+++ OperationData.php
@@ -2,13 +2,13 @@
 $data = [
 	'theme'=>[
 		'icon'=>["ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME"],
-		'text'=>["早起","看书","早睡","AB记单词","喝水","画画","练字","写日记"]
+		'text'=>["早起","看书","早睡","AB记单词","喝水","画画","练字","写日记"],
 		'obj'=>[{
 			check:false,
 			normal:'https://tdream.antit.top/1morningIU.png',
 			active:'https://tdream.antit.top/1morningIA.png',
 			title:'早起'
-		},]
+		}]
 	]
 ];
 ?>
