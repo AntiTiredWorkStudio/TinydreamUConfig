9e1332c92ca989b8f6e503a683987c8ca4d6505e
whugobarry <17600740311@163.com>
2019-04-09 20:11:21 +0800

    代码修改

--- OperationData.php
+++ OperationData.php
@@ -1,8 +1,26 @@
 <?php 
 $data = [
 	'theme'=>[
-		'icon'=>["ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME","ICON_NAME"],
-		'text'=>["早起","看书","早睡","AB记单词","喝水","画画","练字","写日记"]
+		{
+			{
+                check:false,
+                normal:'https://tdream.antit.top/1morningIU.png',
+                active:'https://tdream.antit.top/1morningIA.png',
+                title:'早起'
+			},
+			{
+                check:false,
+                normal:'https://tdream.antit.top/1morningIU.png',
+                active:'https://tdream.antit.top/1morningIA.png',
+                title:'早起'
+			},
+			{
+                check:false,
+                normal:'https://tdream.antit.top/1morningIU.png',
+                active:'https://tdream.antit.top/1morningIA.png',
+                title:'早起'
+            },
+		}
 	]
 ];
 ?>
