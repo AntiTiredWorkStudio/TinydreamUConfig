fbab775b0c8da16e729e1df82e6d46ea7bec5c9b
whugobarry <17600740311@163.com>
2019-04-09 20:49:37 +0800

    代码修改

--- OperationData.php
+++ OperationData.php
@@ -7,6 +7,66 @@ $data = [
 			"active"=>"https://tdream.antit.top/1morningIA.png",
 			"title"=>"早起",
 		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/2readbookIU.png",
+			"active"=>"https://tdream.antit.top/2readbookIA.png",
+			"title"=>"看书",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/3SleepEarlyIU.png",
+			"active"=>"https://tdream.antit.top/3SleepEarlyIA.png",
+			"title"=>"早睡",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/4wordIU.png",
+			"active"=>"https://tdream.antit.top/4wordIA.png",
+			"title"=>"记单词",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/5drinkingIU.png",
+			"active"=>"https://tdream.antit.top/5drinkingIA.png",
+			"title"=>"喝水",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/6drawingIU.png",
+			"active"=>"https://tdream.antit.top/6drawingIA.png",
+			"title"=>"画画",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/7practisecalligraphyIU.png",
+			"active"=>"https://tdream.antit.top/7practisecalligraphyIA.png",
+			"title"=>"练字",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/9breakfastIU.png",
+			"active"=>"https://tdream.antit.top/9breakfastIA.png",
+			"title"=>"吃早餐",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/10fitnessIU.png",
+			"active"=>"https://tdream.antit.top/10fitnessIA.png",
+			"title"=>"健身",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/11loseweightIU.png",
+			"active"=>"https://tdream.antit.top/11loseweightIA.png",
+			"title"=>"减肥",
+		],
+		[
+			"check"=>false,
+			"normal"=>"https://tdream.antit.top/12photographIU.png",
+			"active"=>"https://tdream.antit.top/12photographIA.png",
+			"title"=>"拍照",
+		],
 	]
 ];
 ?>
