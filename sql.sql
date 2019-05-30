5dbe03a4989430c0c5a93d06024a5702ba615de2
whugobarry <17600740311@163.com>
2019-05-30 14:48:58 +0800

    提交测试人员信息

---
+++ sql.sql
@@ -0,0 +1,35 @@
+--查询语句
+UPDATE `user` SET `tele`="" WHERE `uid`="oSORf5kkXvHNxhIx8lQVe3DFRFvw"
+DELETE  FROM `dream` WHERE `uid`="oSORf5kkXvHNxhIx8lQVe3DFRFvw"  
+DELETE  FROM `identityx` WHERE `uid`="oSORf5kkXvHNxhIx8lQVe3DFRFvw"
+UPDATE `operation` SET `state`="FAILED" WHERE `uid`="oSORf5kkXvHNxhIx8lQVe3DFRFvw"
+
+UPDATE `user` SET `tele`="" WHERE `uid`="oSORf5klCAE5_5r-l5CGBwWGR1Vg"
+DELETE  FROM `dream` WHERE `uid`="oSORf5klCAE5_5r-l5CGBwWGR1Vg"  
+DELETE  FROM `identityx` WHERE `uid`="oSORf5klCAE5_5r-l5CGBwWGR1Vg"
+UPDATE `operation` SET `state`="FAILED" WHERE `uid`="oSORf5klCAE5_5r-l5CGBwWGR1Vg"
+
+UPDATE `user` SET `tele`="" WHERE `uid`="oSORf5rBNt4TLnhRgA8f-faJeIKM"
+DELETE  FROM `dream` WHERE `uid`="oSORf5rBNt4TLnhRgA8f-faJeIKM"  
+DELETE  FROM `identityx` WHERE `uid`="oSORf5rBNt4TLnhRgA8f-faJeIKM"
+UPDATE `operation` SET `state`="FAILED" WHERE `uid`="oSORf5rBNt4TLnhRgA8f-faJeIKM"
+
+UPDATE `user` SET `tele`="" WHERE `uid`="oSORf5lxYOuWeUFl--XkarNC4BP4"
+DELETE  FROM `dream` WHERE `uid`="oSORf5lxYOuWeUFl--XkarNC4BP4"  
+DELETE  FROM `identityx` WHERE `uid`="oSORf5lxYOuWeUFl--XkarNC4BP4"
+UPDATE `operation` SET `state`="FAILED" WHERE `uid`="oSORf5lxYOuWeUFl--XkarNC4BP4"
+
+UPDATE `user` SET `tele`="" WHERE `uid`="oSORf5iMOQs0L9pTxJxUAWnTdQhM"
+DELETE  FROM `dream` WHERE `uid`="oSORf5iMOQs0L9pTxJxUAWnTdQhM"  
+DELETE  FROM `identityx` WHERE `uid`="oSORf5iMOQs0L9pTxJxUAWnTdQhM"
+UPDATE `operation` SET `state`="FAILED" WHERE `uid`="oSORf5iMOQs0L9pTxJxUAWnTdQhM"
+
+UPDATE `user` SET `tele`="" WHERE `uid`="oSORf5nKpKgXtVfLUi-lNhS60u0U"
+DELETE  FROM `dream` WHERE `uid`="oSORf5nKpKgXtVfLUi-lNhS60u0U"  
+DELETE  FROM `identityx` WHERE `uid`="oSORf5nKpKgXtVfLUi-lNhS60u0U"
+UPDATE `operation` SET `state`="FAILED" WHERE `uid`="oSORf5nKpKgXtVfLUi-lNhS60u0U"
+
+UPDATE `user` SET `tele`="" WHERE `uid`="oSORf5hkHfOy3Yo4FQIPdbHKQljM"
+DELETE  FROM `dream` WHERE `uid`="oSORf5hkHfOy3Yo4FQIPdbHKQljM"  
+DELETE  FROM `identityx` WHERE `uid`="oSORf5hkHfOy3Yo4FQIPdbHKQljM"
+UPDATE `operation` SET `state`="FAILED" WHERE `uid`="oSORf5hkHfOy3Yo4FQIPdbHKQljM"
